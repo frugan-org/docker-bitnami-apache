c4321b0c1ef1d5bbe4445cc4131230b1086e0311
Frugan <dev@frugan.it>
2021-03-23 12:15:19 +0100

    Moved USER_ID and GROUP_ID in Dockerfile

--- 2.4/entrypoint.sh
+++ 2.4/entrypoint.sh
@@ -7,17 +7,6 @@ set -o nounset
 set -o pipefail
 #set -o xtrace # Uncomment this line for debugging purpose
 
-#https://jtreminio.com/blog/running-docker-containers-as-current-host-user/#ok-so-what-actually-works
-if [ ${USER_ID} -ne 0 ] && [ ${GROUP_ID} -ne 0 ]; then
-  userdel -f daemon;
-  if getent group daemon; then
-      groupdel daemon;
-  fi
-  groupadd -g ${GROUP_ID} daemon;
-  useradd -l -u ${USER_ID} -g daemon daemon;
-  install -d -m 0755 -o daemon -g daemon /home/daemon;
-fi
-
 
 #https://docs.bitnami.com/bch/apps/wordpress/configuration/enable-modules/
 if [ ! -z "${APACHE_REWRITE_MODULE_ENABLED:-}" ]; then

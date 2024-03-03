cb5e15e01862566d849d684215f655dcf81fb85a
Frugan <dev@frugan.it>
2024-03-03 15:38:13 +0100

    feat!: replace ENV with APP_ENV
    shellcheck and lint scripts
    update docs

--- 2.4/entrypoint.sh
+++ 2.4/entrypoint.sh
@@ -1,152 +1,148 @@
 #!/bin/bash
 
-# shellcheck disable=SC1091
-
 set -o errexit
 set -o nounset
 set -o pipefail
 #set -o xtrace # Uncomment this line for debugging purpose
 
-
 #https://docs.bitnami.com/bch/apps/wordpress/configuration/enable-modules/
-if [ ! -z "${APACHE_REWRITE_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*rewrite_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_REWRITE_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*rewrite_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_LDAP_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*mod_authnz_ldap\)/\1/' \
-    -e 's/^#\(LoadModule .*mod_ldap\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_LDAP_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*mod_authnz_ldap\)/\1/' \
+		-e 's/^#\(LoadModule .*mod_ldap\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_EVASIVE_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*evasive20_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_EVASIVE_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*evasive20_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 else
-  sed -i \
-    -e 's/^\(LoadModule .*evasive20_module\)/#\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+	sed -i \
+		-e 's/^\(LoadModule .*evasive20_module\)/#\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_SUBSTITUTE_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*substitute_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_SUBSTITUTE_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*substitute_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
 
 # W3TC -> Page Cache and Browser Cache
-if [ ! -z "${APACHE_DEFLATE_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*deflate_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_DEFLATE_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*deflate_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_ENV_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*env_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_ENV_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*env_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_EXPIRES_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*expires_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_EXPIRES_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*expires_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_EXT_FILTER_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*ext_filter_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_EXT_FILTER_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*ext_filter_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_FILTER_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*filter_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_FILTER_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*filter_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_HEADERS_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*headers_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_HEADERS_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*headers_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_MIME_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*mime_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_MIME_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*mime_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_SETENVIF_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*setenvif_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_SETENVIF_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*setenvif_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-if [ ! -z "${APACHE_REMOTEIP_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*remoteip_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_REMOTEIP_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*remoteip_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
 
 #https://community.bitnami.com/t/unable-to-enable-http-2-support/67229/7
-if [ ! -z "${APACHE_HTTP2_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*http2_module\)/\1/' \
-    -e 's/^\(LoadModule .*mpm_prefork_module\)/#\1/' \
-    -e 's/^#\(LoadModule .*mpm_event_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_HTTP2_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*http2_module\)/\1/' \
+		-e 's/^\(LoadModule .*mpm_prefork_module\)/#\1/' \
+		-e 's/^#\(LoadModule .*mpm_event_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
 
-if [ ! -z "${APACHE_SECURITY3_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*security3_module\)/\1/' \
-    -e 's/^#\(LoadModule .*unique_id_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_SECURITY3_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*security3_module\)/\1/' \
+		-e 's/^#\(LoadModule .*unique_id_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 
-  sed -i \
-    -e 's/\/var\/log\/modsec_audit\.log/\/opt\/bitnami\/apache\/logs\/modsec_audit\.log/' \
-    /opt/bitnami/apache/conf/modsecurity.conf;
+	sed -i \
+		-e 's/\/var\/log\/modsec_audit\.log/\/opt\/bitnami\/apache\/logs\/modsec_audit\.log/' \
+		/opt/bitnami/apache/conf/modsecurity.conf
 
-  ln -sf /dev/stdout /opt/bitnami/apache/logs/modsec_audit.log;
+	ln -sf /dev/stdout /opt/bitnami/apache/logs/modsec_audit.log
 fi
 
 #https://support.cloudflare.com/hc/en-us/articles/360029696071-Restoring-original-visitor-IPs-Option-2-Installing-mod-remoteip-with-Apache
 #https://community.traefik.io/t/restoring-cloudflare-real-ip-on-traefik/7675
 #https://stackoverflow.com/a/63380213/3929620
-if [ ! -z "${APACHE_PAGESPEED_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(Include .*pagespeed\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_PAGESPEED_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(Include .*pagespeed\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
 
-if [ ! -z "${APACHE_AUTOINDEX_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*alias_module\)/\1/' \
-    -e 's/^#\(LoadModule .*authz_core_module\)/\1/' \
-    -e 's/^#\(LoadModule .*authz_host_module\)/\1/' \
-    -e 's/^#\(LoadModule .*autoindex_module\)/\1/' \
-    -e 's/^#\(Include .*httpd-autoindex\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_AUTOINDEX_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*alias_module\)/\1/' \
+		-e 's/^#\(LoadModule .*authz_core_module\)/\1/' \
+		-e 's/^#\(LoadModule .*authz_host_module\)/\1/' \
+		-e 's/^#\(LoadModule .*autoindex_module\)/\1/' \
+		-e 's/^#\(Include .*httpd-autoindex\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
 
 #https://github.com/BytemarkHosting/docker-webdav
 #https://github.com/uGeek/docker-webdav
 #https://github.com/idelsink/webdav-docker
-if [ ! -z "${APACHE_DAV_MODULE_ENABLED:-}" ]; then
-  sed -i \
-    -e 's/^#\(LoadModule .*alias_module\)/\1/' \
-    -e 's/^#\(LoadModule .*auth_digest_module\)/\1/' \
-    -e 's/^#\(LoadModule .*authn_core_module\)/\1/' \
-    -e 's/^#\(LoadModule .*authn_file_module\)/\1/' \
-    -e 's/^#\(LoadModule .*authz_core_module\)/\1/' \
-    -e 's/^#\(LoadModule .*authz_user_module\)/\1/' \
-    -e 's/^#\(LoadModule .*dav_module\)/\1/' \
-    -e 's/^#\(LoadModule .*dav_fs_module\)/\1/' \
-    -e 's/^#\(LoadModule .*setenvif_module\)/\1/' \
-    /opt/bitnami/apache/conf/httpd.conf;
+if [ -n "${APACHE_DAV_MODULE_ENABLED:-}" ]; then
+	sed -i \
+		-e 's/^#\(LoadModule .*alias_module\)/\1/' \
+		-e 's/^#\(LoadModule .*auth_digest_module\)/\1/' \
+		-e 's/^#\(LoadModule .*authn_core_module\)/\1/' \
+		-e 's/^#\(LoadModule .*authn_file_module\)/\1/' \
+		-e 's/^#\(LoadModule .*authz_core_module\)/\1/' \
+		-e 's/^#\(LoadModule .*authz_user_module\)/\1/' \
+		-e 's/^#\(LoadModule .*dav_module\)/\1/' \
+		-e 's/^#\(LoadModule .*dav_fs_module\)/\1/' \
+		-e 's/^#\(LoadModule .*setenvif_module\)/\1/' \
+		/opt/bitnami/apache/conf/httpd.conf
 fi
-
 
 #### h5bp
 
-wget -O /opt/bitnami/apache/conf/h5bp.conf https://raw.githubusercontent.com/h5bp/server-configs-apache/master/dist/.htaccess;
-
+wget -O /opt/bitnami/apache/conf/h5bp.conf https://raw.githubusercontent.com/h5bp/server-configs-apache/master/dist/.htaccess
 
 ####
 
 #https://stackoverflow.com/a/46433245/3929620
+# shellcheck source=/dev/null
 . /opt/bitnami/scripts/apache/entrypoint.sh
--- deploy.sh
+++ deploy.sh
@@ -3,30 +3,33 @@
 #https://blog.dockbit.com/templating-your-dockerfile-like-a-boss-2a84a67d28e9
 
 deploy() {
-  str="
+	str="
   s!%%TAG%%!$TAG!g;
 "
 
-  sed -r "$str" $1
+	sed -r "$str" "$1"
 }
 
 TAGS=(
-  2.4
+	2.4
 )
 
 ENTRYPOINT=entrypoint.sh
 
+IFS='
+'
+# shellcheck disable=SC2048
 for TAG in ${TAGS[*]}; do
 
-  if [ -d "$TAG" ]; then
-    rm -Rf $TAG
-  fi
+	if [ -d "$TAG" ]; then
+		rm -Rf "$TAG"
+	fi
 
-  mkdir $TAG
-  deploy Dockerfile.template > $TAG/Dockerfile
+	mkdir "$TAG"
+	deploy Dockerfile.template >"$TAG"/Dockerfile
 
-  if [ -f "$ENTRYPOINT" ]; then
-    cp $ENTRYPOINT $TAG
-  fi
+	if [ -f "$ENTRYPOINT" ]; then
+		cp $ENTRYPOINT "$TAG"
+	fi
 
 done
